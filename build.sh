b9dca2e407e0a2027faf04c30f5029aeed924325
brinkqiang <brink.qiang@gmail.com>
2025-01-24 14:21:44 +0800

    add

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 echo sudo apt install unixodbc unixodbc-dev
 echo wget https://dev.mysql.com/get/Downloads/Connector-ODBC/9.2/mysql-connector-odbc_9.2.0-1ubuntu22.04_amd64.deb
 echo sudo dpkg -i mysql-connector-odbc_9.2.0-1ubuntu22.04_amd64.deb
+echo cat /etc/odbcinst.ini
 
 mkdir -p build
 cd build
