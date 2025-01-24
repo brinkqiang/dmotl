9ca09ca4151cffaad68e7a7ab07c10774453d6aa
brinkqiang <brink.qiang@gmail.com>
2025-01-24 13:44:22 +0800

    add

--- build.bat
+++ build.bat
@@ -1,6 +1,5 @@
 
-echo sudo apt install unixodbc-dev
-
+echo https://dev.mysql.com/get/Downloads/Connector-ODBC/9.2/mysql-connector-odbc-9.2.0-winx64.msi
 rmdir /S /Q build
 mkdir build
 pushd build
--- build.sh
+++ build.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
-# - install depends tools
-# yum -y install git
-# yum -y install gcc gcc-c++ autoconf libtool automake make
-#
 
-# pushd thirdparty/depends_path
-# libtoolize && aclocal && autoheader && autoconf && automake --add-missing
-# sh configure
-# popd
+echo sudo apt install unixodbc-dev
 
-rm -rf build
 mkdir -p build
-pushd build
+cd build
 
 cmake -DCMAKE_BUILD_TYPE=relwithdebinfo ..
 cmake --build . --config relwithdebinfo
 
-popd
+cd ..
 
 # popd
 
